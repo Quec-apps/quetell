855a167a1a573f0c90126bde89f2471fd423bda7
Quec-apps <79459289+Quec-apps@users.noreply.github.com>
2022-04-17 21:37:08 +0530

    Add files via upload

--- content/res/script/var/aesop.js
+++ content/res/script/var/aesop.js
@@ -5,7 +5,55 @@ window["aesop_ti"+x] = "The Wolf and the Lamb";
 window["aesop_li"+x] = 2.6;
 
 x++;
-window["aesop_ti"+x] = "";
+window["aesop_ti"+x] = "The Dog and the Shadow";
+window["aesop_li"+x] = 2.6;
+
+x++;
+window["aesop_ti"+x] = "The Lion’s Share";
+window["aesop_li"+x] = 2.6;
+
+x++;
+window["aesop_ti"+x] = "The Wolf and the Crane";
+window["aesop_li"+x] = 2.6;
+
+x++;
+window["aesop_ti"+x] = "The Man and the Serpent";
+window["aesop_li"+x] = 2.6;
+
+x++;
+window["aesop_ti"+x] = "The Town Mouse and the Country Mouse";
+window["aesop_li"+x] = 2.6;
+
+x++;
+window["aesop_ti"+x] = "The Fox and the Crow";
+window["aesop_li"+x] = 2.6;
+
+x++;
+window["aesop_ti"+x] = "The Sick Lion";
+window["aesop_li"+x] = 2.6;
+
+x++;
+window["aesop_ti"+x] = "The Ass and the Lapdog";
+window["aesop_li"+x] = 2.6;
+
+x++;
+window["aesop_ti"+x] = "The Lion and the Mouse";
+window["aesop_li"+x] = 2.6;
+
+x++;
+window["aesop_ti"+x] = "The Swallow and the Other Birds";
+window["aesop_li"+x] = 2.6;
+
+x++;
+window["aesop_ti"+x] = "The Frogs Desiring a King";
+window["aesop_li"+x] = 2.6;
+
+x++;
+window["aesop_ti"+x] = "The Mountains in Labour";
+window["aesop_li"+x] = 2.6;
+
+x++;
+window["aesop_ti"+x] = "The Hares and the Frogs";
 window["aesop_li"+x] = 2.6;
 
 x++;
@@ -40,55 +88,7 @@ x++;
 window["aesop_ti"+x] = "";
 window["aesop_li"+x] = 2.6;
 
-x++;
-window["aesop_ti"+x] = "";
-window["aesop_li"+x] = 2.6;
-
-x++;
-window["aesop_ti"+x] = "";
-window["aesop_li"+x] = 2.6;
-
-x++;
-window["aesop_ti"+x] = "";
-window["aesop_li"+x] = 2.6;
-
-x++;
-window["aesop_ti"+x] = "";
-window["aesop_li"+x] = 2.6;
-
-x++;
-window["aesop_ti"+x] = "";
-window["aesop_li"+x] = 2.6;
-
-x++;
-window["aesop_ti"+x] = "";
-window["aesop_li"+x] = 2.6;
-
-x++;
-window["aesop_ti"+x] = "";
-window["aesop_li"+x] = 2.6;
-
-x++;
-window["aesop_ti"+x] = "";
-window["aesop_li"+x] = 2.6;
-
-x++;
-window["aesop_ti"+x] = "";
-window["aesop_li"+x] = 2.6;
-
-x++;
-window["aesop_ti"+x] = "";
-window["aesop_li"+x] = 2.6;
-
-x++;
-window["aesop_ti"+x] = "";
-window["aesop_li"+x] = 2.6;
-
-x++;
-window["aesop_ti"+x] = "";
-window["aesop_li"+x] = 2.6;
 
 
-
-aesop_total = 5;
+aesop_total = 14;
 localStorage.setItem("aesop_total", aesop_total);
---
+++ content/res/script/var/panchatantra.js
@@ -0,0 +1,190 @@
+x = 0;
+
+x++;
+window["panchatantra_ti"+x] = "Who will Bell the Cat?";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "How Monkeys Got Their Red Bottoms";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "The Lion Cub";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "The Monkey and The Crocodile";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "The Heron And The Crab";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "The Groomed Horse";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "The Geese and The Tortoise";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "The Story of the Blue Jackal";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "The Merchant’s Son";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "The Fox And The Goat";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "The Groomed Horse";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "The Lion’s Share";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "The Donkey Monkeys Around";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "The Horse’s Revenge";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "The Devta And The Weaver";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "Somilaka The Weaver";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "King Nanda And Vararuchi";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "Rich Mohan And Poor Sohan";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "The Merchant And The Barber";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "The Giant And The Horse Thief";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "";
+window["panchatantra_li"+x] = 2.6;
+
+x++;
+window["panchatantra_ti"+x] = "";
+window["panchatantra_li"+x] = 2.6;
+
+
+
+panchatantra_total = 20;
+localStorage.setItem("panchatantra_total", panchatantra_total);
